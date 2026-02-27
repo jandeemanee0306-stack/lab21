7a9ddb78428cf8301deb98906371ba771b9fb48c
Siriphum <Jandeemanee0306@gmail.com>
2026-02-27 15:12:47 +0700

    average.cpp completed

--- average.cpp
+++ average.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>  // atof
+using namespace std;
 
+int main(int argc, char* argv[]) {
+
+    if (argc == 1) {
+        cout << "Please input numbers to find average." << endl;
+        return 0;
+    }
+
+    int sum = 0;
+
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]);   // แปลง string เป็น double
+    }
+
+    int average = sum / (argc - 1);
+
+    cout << "---------------------------------" << endl;
+    cout << "Average of " << (argc - 1) << " numbers = "
+         << fixed << setprecision(3) << average << endl;
+    cout << "---------------------------------" << endl;
+
+    return 0;
+}
